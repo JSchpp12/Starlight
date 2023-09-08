c7885800ac797dde5457ee1924cabbf2eefdfdc4
Jacob Schaupp <jacob.schaupp@gmail.com>
2023-09-07 21:58:25 -0400

    Externs

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+int main(){
+    std::cout << "Hello World! One Day I Will be a Rendering Engine! Maybe even used for some games!" << std::endl; 
+}
