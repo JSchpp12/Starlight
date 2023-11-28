49a656f0991c0e62500d2f2a70383ee64ff15d6e
Jacob Schaupp <jacob.schaupp@gmail.com>
2023-11-27 19:25:17 -0500

    Fix for buffer bug

--- src/common/objects/StarObjectInstance.cpp
+++ src/common/objects/StarObjectInstance.cpp
@@ -5,11 +5,12 @@ void star::StarObjectInstance::updateBufferData(StarBuffer& buffer, int bufferIn
 	switch (bufferIndex) {
 	case(0):
 		auto displayMatrix = this->getDisplayMatrix();
-		buffer.writeToIndex(&displayMatrix, this->instanceIndex); 
+		buffer.writeToBuffer(&displayMatrix, sizeof(displayMatrix), sizeof(displayMatrix) * this->instanceIndex);
 		break;
 	case(1):
 		auto normalMatrix = glm::inverseTranspose(getDisplayMatrix());
-		buffer.writeToIndex(&normalMatrix, this->instanceIndex);
+		//buffer.writeToIndex(&normalMatrix, this->instanceIndex);
+		buffer.writeToBuffer(&displayMatrix, sizeof(normalMatrix), sizeof(normalMatrix) * this->instanceIndex); 
 		break;
 	}
 }
