35cae23b17049ebca8cc176938810c0c81e7f9b7
Jacob Schaupp <jacob.schaupp@gmail.com>
2024-01-12 13:03:11 -0500

    Color update

--- include/structs/Color.hpp
+++ include/structs/Color.hpp
@@ -4,22 +4,43 @@ namespace star {
 	class Color {
 	public:
 		Color() = default; 
-		Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a) : rr(r), rg(g), rb(b), ra(a) {}; 
-		Color(int r, int g, int b, int a) : rr(r), rg(g), rb(b), ra(a) {};
+		Color(unsigned char r, unsigned char g, unsigned char b, unsigned char a) : rr(255.0f / r), rg(255.0f / g), rb(255.0f / b), ra(255.0f / a) {};
+		Color(float r, float g, float b, float a) : rr(r), rg(g), rb(b), ra(a) {};
 
-		//void r(int r) { rr = r; }
-		//void g(int g) { rg = g; }
-		//void b(int b) { rb = b; }
-		int r() { return int(rr); }
-		int g() { return int(rg); }
-		int b() { return int(rb); }
-		int a() { return int(ra); }
-		unsigned char raw_r() { return rr; }
-		unsigned char raw_g() { return rg; }
-		unsigned char raw_b() { return rb; }
-		unsigned char raw_a() { return ra; }
+		void setR(const float& r){
+			if (r <= 1.0f)
+				this->rr = unsigned char(255 * r);
+			else
+				this->rr = unsigned char(255); 
+		}
+		void setG(const float& g) {
+			if (g <= 1.0f)
+				this->rg = unsigned char(255 * g);
+			else
+				this->rg = unsigned char(255);
+		}
+		void setB(const float& b) {
+			if (b <= 1.0f)
+				this->rb = unsigned char(255 * b);
+			else
+				this->rb = unsigned char(255);
+		}
+		void setA(const float& a) {
+			if (a <= 1.0f)
+				this->ra = unsigned char(255 * a);
+			else
+				this->ra = unsigned char(255);
+		}
+		int r() const { return int(rr * 255); }
+		int g() const { return int(rg * 255); }
+		int b() const { return int(rb * 255); }
+		int a() const { return int(ra * 255); }
+		unsigned char raw_r() const { return unsigned char(255 * rr); }
+		unsigned char raw_g() const { return unsigned char(255 * rg); }
+		unsigned char raw_b() const { return unsigned char(255 * rb); }
+		unsigned char raw_a() const { return unsigned char(255 * ra); }
 
 	protected:
-		unsigned char rr, rg, rb, ra;
+		float rr = 0.0f, rg = 0.0f, rb = 0.0f, ra = 0.0f;
 	};
 }
