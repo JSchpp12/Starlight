d858e75602674c5b6d653e740ababbaf8a75d5cb
Jacob Schaupp <jacob.schaupp@gmail.com>
2024-08-20 10:21:57 -0400

    Fix for incorrect texture storage

--- src/StarEngine.cpp
+++ src/StarEngine.cpp
@@ -1,9 +1,5 @@
 #include "StarEngine.hpp"
 
-#define STB_IMAGE_IMPLEMENTATION   
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image.h"
-
 namespace star {
 
 std::unique_ptr<std::string> StarEngine::screenshotPath = nullptr;
