4991e87563b335206c6635ee459f3deda9efd4d2
Jacob Schaupp <jacob.schaupp@gmail.com>
2025-06-26 15:29:53 -0400

    QUICK_FIX: Star Engine Delete Order

--- include/StarEngine.hpp
+++ include/StarEngine.hpp
@@ -22,11 +22,12 @@ class StarEngine
     void run();
 
   protected:
-    std::unique_ptr<StarApplication> application = nullptr; 
     std::unique_ptr<StarWindow> window = nullptr;
     std::unique_ptr<StarDevice> renderingDevice = nullptr;
+    std::unique_ptr<StarApplication> application = nullptr; 
     std::shared_ptr<SwapChainRenderer> mainRenderer = nullptr;
     std::unique_ptr<TransferWorker> transferWorker = nullptr;
+    
 
   private:
     const bool OVERRIDE_APPLY_SINGLE_THREAD_MODE = false;
