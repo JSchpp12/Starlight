7e07a8e57bc0ba154d2337910bdf163a47f096cd
Jacob Schaupp <jacob.schaupp@gmail.com>
2025-10-24 11:31:44 -0400

    Formatting

--- include/core/MappedHandleContainer.hpp
+++ include/core/MappedHandleContainer.hpp
@@ -1,56 +1,59 @@
 #pragma once
 
-#include "HandleContainer.hpp"
 #include "CastHelpers.hpp"
+#include "HandleContainer.hpp"
 
 #include <unordered_map>
 
 namespace star::core
 {
-template<typename TData, star::Handle_Type THandleType>
+template <typename TData, star::Handle_Type THandleType>
 class MappedHandleContainer : public HandleContainer<TData, THandleType>
 {
-    public:
-    void manualInsert(const Handle &handle, TData record){
+  public:
+    void manualInsert(const Handle &handle, TData record)
+    {
         store(handle, std::move(record));
     }
-    
-    protected:
-    Handle storeRecord(TData newData) override{
-        Handle insertHandle = createNewHandle(); 
-        store(insertHandle, std::move(newData)); 
-        
+
+  protected:
+    Handle storeRecord(TData newData) override
+    {
+        Handle insertHandle = createNewHandle();
+        store(insertHandle, std::move(newData));
+
         return insertHandle;
     }
 
-    TData &getRecord(const Handle &handle) override{
+    TData &getRecord(const Handle &handle) override
+    {
         assert(m_records.contains(handle) && "Handle must be in the set");
-        return m_records[handle]; 
+        return m_records[handle];
     }
-    void removeRecord(const Handle &handle, device::StarDevice *device) override{
+    void removeRecord(const Handle &handle, device::StarDevice *device) override
+    {
         assert(m_records.contains(handle) && "Handle must be in the set");
 
         m_records.erase(handle);
     }
 
-    private:
+  private:
     std::unordered_map<Handle, TData, star::HandleHash> m_records;
 
-    Handle createNewHandle() const{
+    Handle createNewHandle() const
+    {
         uint32_t newId = 0;
-        CastHelpers::SafeCast<size_t, uint32_t>(m_records.size(), newId); 
+        CastHelpers::SafeCast<size_t, uint32_t>(m_records.size(), newId);
 
-        Handle newHandle {
-            .type = THandleType,
-            .id = std::move(newId)
-        };
+        Handle newHandle{.type = THandleType, .id = std::move(newId)};
 
         return newHandle;
     }
 
-    void store(const Handle &recordHandle, TData record){
-        assert(recordHandle.getType() == THandleType && "Ensure proper handle type for container"); 
-        assert(!m_records.contains(recordHandle) && "Handle must be unique and not already used"); 
+    void store(const Handle &recordHandle, TData record)
+    {
+        assert(recordHandle.getType() == THandleType && "Ensure proper handle type for container");
+        assert(!m_records.contains(recordHandle) && "Handle must be unique and not already used");
 
         m_records.insert(std::make_pair(recordHandle, std::move(record)));
     }
