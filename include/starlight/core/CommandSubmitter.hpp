1cfbf4f8b4f42e25aaa57debd4c4a31ac7a2edc2
Jacob Schaupp <jacob.schaupp@gmail.com>
2026-01-26 13:00:31 -0500

    Add comment for clarity

--- include/starlight/core/CommandSubmitter.hpp
+++ include/starlight/core/CommandSubmitter.hpp
@@ -21,6 +21,9 @@ template <typename TDeviceContext> class CommandSubmitter
         m_command = &command;
         return *this;
     }
+    /// @brief Manually define a unique name for the type of command to submit.
+    /// @param name The unique name to be used in lookups for this type of command
+    /// @return 
     CommandSubmitter &setName(std::string_view name)
     {
         m_name = name;
